455d9bf82154cc1f68326f99b1667ca8b593c0e7
thingsastray <andrewpeterson808@gmail.com>
2015-11-06 13:12:25 -1000

    fixed the finish

--- secret_number.js
+++ secret_number.js
@@ -1,9 +1,9 @@
-var max = 1000000;
-var min = 0;
-var number;
+
 
 'use strict';
 module.exports = function() {
+	var max = 1000000;
+var min = 0;
 	var number = Math.random() * (max - min) + min;
 	var secretNumber = function(max, min){
 		return number;
