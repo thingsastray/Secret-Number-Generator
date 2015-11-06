71e22ea88c7f6dac3435dc0d194c5cf0ba329060
thingsastray <andrewpeterson808@gmail.com>
2015-11-05 21:31:13 -1000

    finished

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,12 @@
+var max = 1000000;
+var min = 0;
+var number;
+
 'use strict';
 module.exports = function() {
-  
+	var number = Math.random() * (max - min) + min;
+	var secretNumber = function(max, min){
+		return number;
+	};	
+	return secretNumber;
 };
